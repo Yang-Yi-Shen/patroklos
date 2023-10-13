ae09d2a1218555b920b6f48f43cd6adfc2a9b684
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-10-13 15:20:18 +0800

    feat: send email on order

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,12 @@ import MenuItem from "/src/components/MenuItem.vue";
 import Footer from "/src/components/Footer.vue";
 
 const orderLength = ref(0)
+const orderMessage = ref(localStorage.getItem('order') || 'No items selected')
+
+function orderItem() {
+  orderMessage.value = localStorage.getItem('order')
+  orderLength.value += 1
+}
 </script>
 
 <template>
@@ -13,17 +19,20 @@ const orderLength = ref(0)
   <main>
     <h1 class="main-dishes-title heading">Main Dishes</h1>
     <div class="dish-list">
-      <MenuItem @item-ordered="orderLength += 1" image="/gyros.png" name="Lamb Gyros" description="Lamp meat wrapped with thick Greek bread" price="6.99" />
-      <MenuItem @item-ordered="orderLength += 1" image="/moussaka.png" name="Moussaka" description="A Greek version of Italian Lasagna, using lamb meat" price="6.99" />
-      <MenuItem @item-ordered="orderLength += 1" image="/souvlaki.png" name="Souvlaki" description="Roasted meats on a skewer, accompanied by light herbs" price="4.20" />
+      <MenuItem @item-ordered="orderItem" image="/gyros.png" name="Lamb Gyros" description="Lamp meat wrapped with thick Greek bread" price="6.99" />
+      <MenuItem @item-ordered="orderItem" image="/moussaka.png" name="Moussaka" description="A Greek version of Italian Lasagna, using lamb meat" price="6.99" />
+      <MenuItem @item-ordered="orderItem" image="/souvlaki.png" name="Souvlaki" description="Roasted meats on a skewer, accompanied by light herbs" price="4.20" />
     </div>
     <h1 class="additional-dishes-title heading">Additional Dishes</h1>
     <div class="dish-list">
-      <MenuItem @item-ordered="orderLength += 1" image="/ouzo.png" name="Ouzo" description="Dense alchoholic beverage made from grapes" price="6.99" />
-      <MenuItem @item-ordered="orderLength += 1" image="/kataifi.png" name="Kataifi" description="A thready, crunchy greek confection" price="4.20" />
-      <MenuItem @item-ordered="orderLength += 1" image="/yogurt.png" name="Greek Yogurt" description="Sour yogurt without artificial seasoning" price="4.20" />
+      <MenuItem @item-ordered="orderItem" image="/ouzo.png" name="Ouzo" description="Dense alchoholic beverage made from grapes" price="6.99" />
+      <MenuItem @item-ordered="orderItem" image="/kataifi.png" name="Kataifi" description="A thready, crunchy greek confection" price="4.20" />
+      <MenuItem @item-ordered="orderItem" image="/yogurt.png" name="Greek Yogurt" description="Sour yogurt without artificial seasoning" price="4.20" />
     </div>
   </main>
+  <div class="order">
+    <a class="order-btn heading" :href="`mailto:mrshenyangyi@gmail.com?subject=Restaurant order&body=${orderMessage}%0D%0A%0D%0AWhile this is just an email with the order contents, you can see you this would translate to integration with an actual restaurant's ordering system.%0D%0AYou can replace the fake email with yours to have it sent to your inbox, if you wish.`">Place your order</a>
+  </div>
   <div class="contact">
     <h1 class="contact-title heading">Contact Us</h1>
     <p class="contact-subtitle">Check out our social media accounts, or directly call us at 06-969-420</p>
@@ -47,7 +56,7 @@ const orderLength = ref(0)
 
 <style scoped>
 main {
-  padding: 50px 200px;
+  padding: 50px 200px 50px;
 }
 
 .dish-list {
@@ -82,6 +91,29 @@ main {
 
 .dish-price {
   margin: 0px;
+}
+
+.order {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 100px;
+}
+
+.order-btn {
+  margin: 0px auto;
+  padding: 10px 20px;
+  font-size: 22px;
+  border-radius: 5px;
+  border: none;
+  background-color: var(--heading);
+  color: var(--background);
+  transition: background-color 0.2s;
+  text-decoration: none;
+}
+
+.order-btn:hover {
+  background-color: var(--highlight);
 }
 
 /* contact section */
